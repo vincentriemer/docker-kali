fb57fb92ce2169fc2520715be3a3aac2ddefe7a0
Vincent Riemer <vincentriemer@gmail.com>
2017-03-27 10:22:07 -0400

    fix stage-2 build

--- stage-2/build.sh
+++ stage-2/build.sh
@@ -7,8 +7,15 @@ CONFIG_BUILDER_TAG="vincentriemer/kali:config-builder"
 
 docker build -t $CONFIG_BUILDER_TAG $SDIR
 
-docker run --rm -v $SDIR:/usr/src/app $CONFIG_BUILDER_TAG ./generate-kali-tools-info.sh
-docker run --rm -v $SDIR:/usr/src/app $CONFIG_BUILDER_TAG yarn && ./generate.js
+docker run --rm \
+    -v $SDIR:/usr/src/app \
+    $CONFIG_BUILDER_TAG \
+    bash -c "./generate-kali-tools-info.sh"
+
+docker run --rm \
+    -v $SDIR:/usr/src/app \
+    $CONFIG_BUILDER_TAG \
+    bash -c "npm cache clean && npm i && ./generate.js"
 
 ./build-package-images.sh
 
